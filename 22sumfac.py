842af776a1a0592ebe37e5e6a7a752c17f2b51e1
Johnny Vertiz <verti@TacoBook-Pro.local>
2023-01-28 14:11:28 -0800

    new

---
+++ 22sumfac.py
@@ -0,0 +1,20 @@
+# 22sumfac.py
+
+# Write a program that computes the running sum from 1..n
+# Also, have it compute the factorial of n while you're at it
+# Use the same loop for both calculations
+
+# Note: you may not import math or any other library
+
+n = 5 # use this value for your computation
+rsum = 0
+fac = 1
+for i in range(1, n+1):
+	rsum += i
+	fac *=i
+
+print(n,rsum,fac)
+"""
+python3 22sumfac.py
+5 15 120
+"""
