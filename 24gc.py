46c5fa850b4016a65e142e133fa806536c663513
Johnny Vertiz <verti@TacoBook-Pro.local>
2023-02-05 21:25:11 -0800

    init

---
+++ 24gc.py
@@ -0,0 +1,15 @@
+# 24gc.py
+
+# Write a program that computes the GC% of a DNA sequence
+# Format the output for 2 decimal places
+
+dna = 'ACAGAGCCAGCAGATATACAGCAGATACTAT'
+count = 0 
+for nt in dna:
+	if nt == 'G' or nt == 'C':
+		count +=1
+print(f'{count/len(dna):.2f}')
+"""
+python3 24gc.py
+0.42
+"""
