cb97989cc8f91d5ff5c1c3b1085f57b0e7734139
Johnny Vertiz <verti@campus-117-015.ucdavis.edu>
2023-02-17 16:18:52 -0800

    init

--- 30stats.py
+++ 30stats.py
@@ -7,8 +7,41 @@
 
 # Note: you are not allowed to import any library except sys
 
+import sys
+
+vals = []
+for thing in sys.argv[1:]:
+	vals.append(float(thing))
+
+vals.sort() # sorts the numbers 
+
+N = len(vals)
+Min = vals[0]
+Max = vals[-1]
+mean = sum(vals)/len(vals)# find the mean or average 
+
+# Median
+if N % 2 == 1:# this part is in case you have an even number of values
+	Median = vals[N//2]
+else: # this is in case you have an odd number of values
+	Median = ((vals[N//2] + vals[N//2-1])/2)
+
+#std_dev
+variance = 0
+for value in vals:	
+	variance += (value - mean)**2/N 
+	
+std_dev = variance ** 0.5
+	
+print('Count:', N)
+print('Minimum:', Min)
+print('Maximum:', Max)
+print('Mean', f'{mean:.3f}')
+print('Std. dev:', f'{std_dev:.3f}')
+print('Median', f'{Median:.3f}')
 
 """
+
 python3 30stats.py 3 1 4 1 5
 Count: 5
 Minimum: 1.0
