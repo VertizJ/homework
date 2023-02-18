befd0e0aae58c39714b514f530a9c9871c7cb025
Johnny Vertiz <verti@campus-117-015.ucdavis.edu>
2023-02-17 16:21:07 -0800

    init

--- 31entropy.py
+++ 31entropy.py
@@ -8,7 +8,28 @@
 # Note: make sure the probabilities sum to 1.0
 # Note: import math and use the math.log2()
 
+# SUM P*LogP
+#P = [0.1, 0.2, 0.3, 0.4]
+# H = 0
+# for i in range(len(p))
 # Hint: try breaking your program with erroneous input
+
+import math
+import sys
+
+p = []
+# make command lines values into list first
+for vals in sys.argv[1:]:
+	p.append(float(vals))
+p.sort()
+
+# now do the entropy thing
+h = 0
+for i in range(len(p)):
+	h -= p[i] * math.log2(p[i])
+print(f'{h:.3f}')
+
+
 
 
 """
