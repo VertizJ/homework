50b4f203f23e1a26f51bc58d7e76c064e91d80fe
Johnny Vertiz <verti@campus-117-015.ucdavis.edu>
2023-02-17 16:54:47 -0800

    init

--- 32xcoverage.py
+++ 32xcoverage.py
@@ -14,6 +14,33 @@
 
 # Note: you will not get exactly the same results as the command line below
 
+import random
+import sys
+
+genome_size = int(sys.argv[1])
+read_num = int(sys.argv[2])
+read_len = int(sys.argv[3])
+
+fragment = []
+
+for i in range(genome_size):
+	fragment.append(0)
+	
+for j in range(read_num):
+	start = random.randint(0, genome_size - read_len)
+	
+	for k in range(read_len):
+		fragment[start + k] += 1
+		
+print(fragment)
+
+# you do fragment[read_len:-read_len] so that you dont undersample the ends
+mi = min(fragment[read_len:-read_len])
+ma = max(fragment[read_len:-read_len])
+su = sum(fragment[read_len:-read_len])/(genome_size-2*read_len)
+#													^^ 2*read_len account for the ends you didnt include
+
+print(f'{mi}, {ma}, {su:.5f}')
 
 """
 python3 32xcoverage.py 1000 100 100
